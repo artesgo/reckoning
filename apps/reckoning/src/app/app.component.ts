3593672e0a17b53658c2815a38ab0c71065cae19
artesgo <artesgo@gmail.com>
2021-01-23 02:22:42 -0500

    initial commit

---
+++ apps/reckoning/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'artesgo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'reckoning';
+}
